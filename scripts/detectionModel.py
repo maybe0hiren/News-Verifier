5bf1fd2c1b67bb6091185349d35204afaa1b86d1
maybe0hiren <hirenwaghmare25@gmail.com>
2025-07-11 02:10:26 +0530

    Image Processing Prototype initial commit

---
+++ scripts/detectionModel.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as tnn 
+from torchvision import models
+import torch.optim as opt 
+
+def detectionModel(outputs=2):
+    classifierModel = models.resnet18(pretrained=True)
+
+    for x in classifierModel.parameters:
+        x.requires_grad = True
+    
+    finalLayerInputSize = classifierModel.fc.in_features
+    classifierModel.fc = tnn.Linear(finalLayerInputSize, outputs)
+
+    return classifierModel
